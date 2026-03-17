bb0cbaccce2658c65a033addc924eb0585bb436d
King丶 <61230639+564262328@users.noreply.github.com>
2026-03-18 04:11:45 +0800

    Update config.py

--- market_analysis/src/config.py
+++ market_analysis/src/config.py
@@ -1,7 +1,6 @@
-# src/config.py
 DB_FILE = "stocks_db.json"
+TENCENT_URL = "http://qt.gtimg.cn"
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 ]
-TENCENT_URL = "http://qt.gtimg.cn"
